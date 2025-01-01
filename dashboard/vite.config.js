8f9db54ae3ea9e65bc08ae1740c41897be12c167
tanmoysrt <57363826+tanmoysrt@users.noreply.github.com>
2025-01-02 00:33:45 +0530

    chore(vite): configure vite proxy for easy dev env

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -5,6 +5,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dashboard/',
+  server: {
+    proxy: {
+     // Proxy requests not starting with /dashboard to localhost:3333
+     '^/(?!dashboard).*': {
+        target: 'http://localhost:3333',
+        changeOrigin: true,
+        rewrite: (path) => path,
+      },
+    },
+  },
   plugins: [
     vue(),
   ],
